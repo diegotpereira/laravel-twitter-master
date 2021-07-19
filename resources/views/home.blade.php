513a491156e3501c04a3a85b39570ec391de8154
Diego Pereira <diegoteixeirapereira@hotmail.com>
2021-07-18 23:04:31 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            Formulário de Tweet
+        </div>
+        <div class="col-md-8">
+            Linhas do tempo
+        </div>
+    </div>
+</div>
+@endsection

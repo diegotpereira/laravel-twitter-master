c27db07f032ff8ddd95d6b2334dd47d2e50a7d12
Diego Pereira <diegoteixeirapereira@hotmail.com>
2021-07-19 00:01:01 -0300

    criação dos arquivos vue.js.

---
+++ resources/js/assets/js/components/FormComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="col-md-4">
+      <form @submit.prevent="salvarTweet">
+          <div class="form-group">
+              <textarea class="form-control" rows="8" cols="8" maxlength="130" v-model="body" required></textarea>
+          </div>
+          <div class="form-group">
+              <button class="btn btn-primary">
+                  Tweet 
+              </button>    
+          </div>
+      </form>
+  </div>
+</template>
+
+<script>
+
+import Event from '../event.js';
+export default {
+
+    data() {
+        return {
+            body: '',
+            postagemData: {} 
+        }
+    },
+
+    methods: {
+        salvarTweet() {
+            axios.post('/tweet/salvar', {body: this.body}).then(res => {
+                console.log(res.data);
+                Event.$emit('added_tweet', this.postagemData);
+            }).catch(e => {
+                console.log(e);
+            });
+            this.body = '';
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/assets/js/components/TimelineComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="col-md-8 postagens">
+      <p v-if="!postagens.length">Sem postagens</p>
+      <div class="media" v-for="postagem in postagens" :key="postagem.id">
+          <img class="mr-3"/>
+          <div class="media-body">
+              <div class="mt-3">
+                  <a href="#">{{postagem.user.name}}</a>
+              </div>
+              <p>{{postagem.body}}</p>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import Event from '../event.js';
+export default {
+    data() {
+        return {
+            postagens:[],
+            postagem: {}
+        }
+    },
+
+    mounted() {
+        Event.$on('added_tweet', (postagem) => {
+            this.postagens.unshift(postagem);
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
